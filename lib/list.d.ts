ccac2dea71e3174453fd026883146eae18ce703f
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-08-20 09:39:50 +0200

    Initial commit

---
+++ lib/list.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+interface IMembershipListRole {
+    id: string;
+    name: string;
+}
+interface IMembershipListProps {
+    entity: string;
+    entityID: string;
+    roles?: IMembershipListRole[];
+}
+interface IMembershipListState {
+    inviting: boolean;
+}
+export declare class MembershipList extends React.Component<IMembershipListProps, IMembershipListState> {
+    state: IMembershipListState;
+    render(): JSX.Element;
+    private rolesSelect;
+}
+export {};
